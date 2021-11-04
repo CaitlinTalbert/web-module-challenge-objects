51da1e35c9280721547b616d709a079dd60a0f4b
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2021-11-04 15:37:56 -0500

    solved task 4

--- index.js
+++ index.js
@@ -88,15 +88,14 @@ Using the reviews array above:
    }
  }
 
-
-
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Reyna's feedback is missing! Use what you know to do the following: (no function needed) 
   1. Add this feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
   2. log the reviews array to the console to check your work
 */
 
-
+reviews[7].feedback = "this place is chill with really cool people, great for getting work done on weekdays";
+console.log(reviews); 
 
 
 
